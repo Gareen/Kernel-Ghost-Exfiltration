fe7d03639198697595bc284bc07ae57d654c95d6
Gareen <1563567692@qq.com>
2026-03-20 16:52:57 +0800

    Implement enhanced eBPF kprobe multi-attach capabilities and expand test coverage for syscall and cgroup tracing subsystems.

---
+++ cpu.go
@@ -0,0 +1,17 @@
+package Kernel-Ghost-Exfil
+
+// PossibleCPU returns the max number of CPUs a system may possibly have
+// Logical CPU numbers must be of the form 0-n
+func PossibleCPU() (int, error) {
+	return possibleCPU()
+}
+
+// MustPossibleCPU is a helper that wraps a call to PossibleCPU and panics if
+// the error is non-nil.
+func MustPossibleCPU() int {
+	cpus, err := PossibleCPU()
+	if err != nil {
+		panic(err)
+	}
+	return cpus
+}
