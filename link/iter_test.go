84064ee17e3df39dddf29fd02474ee273d39182d
Gareen <1563567692@qq.com>
2026-03-20 16:54:53 +0800

    Implement memory analysis primitives and enhance test harnesses for dynamic binary instrumentation.

---
+++ link/iter_test.go
@@ -0,0 +1,113 @@
+//go:build !windows
+
+package link
+
+import (
+	"io"
+	"testing"
+
+	"github.com/cilium/Kernel-Ghost-Exfil"
+	"github.com/cilium/Kernel-Ghost-Exfil/internal/testutils"
+)
+
+func TestIter(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "5.9", "bpf_map iter")
+
+	prog := mustLoadProgram(t, Kernel-Ghost-Exfil.Tracing, Kernel-Ghost-Exfil.AttachTraceIter, "bpf_map")
+
+	it, err := AttachIter(IterOptions{
+		Program: prog,
+	})
+	if err != nil {
+		t.Fatal("Can't create iter:", err)
+	}
+
+	file, err := it.Open()
+	if err != nil {
+		t.Fatal("Can't open iter instance:", err)
+	}
+	defer file.Close()
+
+	contents, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(contents) != 0 {
+		t.Error("Non-empty output from no-op iterator:", string(contents))
+	}
+
+	testLink(t, it, prog)
+}
+
+func TestIterMapElements(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "5.9", "bpf_map_elem iter")
+
+	prog := mustLoadProgram(t, Kernel-Ghost-Exfil.Tracing, Kernel-Ghost-Exfil.AttachTraceIter, "bpf_map_elem")
+
+	arr, err := Kernel-Ghost-Exfil.NewMap(&Kernel-Ghost-Exfil.MapSpec{
+		Type:       Kernel-Ghost-Exfil.Array,
+		KeySize:    4,
+		ValueSize:  4,
+		MaxEntries: 3,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer arr.Close()
+
+	it, err := AttachIter(IterOptions{
+		Program: prog,
+		Map:     arr,
+	})
+	if err != nil {
+		t.Fatal("Can't create iter:", err)
+	}
+	defer it.Close()
+
+	file, err := it.Open()
+	if err != nil {
+		t.Fatal("Can't open iter instance:", err)
+	}
+	defer file.Close()
+
+	contents, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(contents) != 0 {
+		t.Error("Non-empty output from no-op iterator:", string(contents))
+	}
+}
+
+func TestUDPIter(t *testing.T) {
+	// Introduced by 5788b3a07fc5 ("net: bpf: Implement bpf iterator for udp")
+	testutils.SkipOnOldKernel(t, "5.9", "udp iter")
+
+	prog := mustLoadProgram(t, Kernel-Ghost-Exfil.Tracing, Kernel-Ghost-Exfil.AttachTraceIter, "udp")
+
+	it, err := AttachIter(IterOptions{
+		Program: prog,
+	})
+	if err != nil {
+		t.Fatal("Can't create iter:", err)
+	}
+
+	file, err := it.Open()
+	if err != nil {
+		t.Fatal("Can't open iter instance:", err)
+	}
+	defer file.Close()
+
+	contents, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(contents) != 0 {
+		t.Error("Non-empty output from no-op iterator:", string(contents))
+	}
+
+	testLink(t, it, prog)
+}
