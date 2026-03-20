562f1a078e8b7346b5735b65fd90a4ef63185bd8
Gareen <1563567692@qq.com>
2026-03-20 16:58:40 +0800

    Implement eBPF struct_ops subsystem and signal handling tests for cross-platform compatibility.

---
+++ link/tcx_test.go
@@ -0,0 +1,92 @@
+//go:build !windows
+
+package link
+
+import (
+	"fmt"
+	"math"
+	"net"
+	"testing"
+
+	"github.com/go-quicktest/qt"
+
+	"github.com/cilium/Kernel-Ghost-Exfil"
+	"github.com/cilium/Kernel-Ghost-Exfil/internal/testutils"
+	"github.com/cilium/Kernel-Ghost-Exfil/internal/unix"
+)
+
+func TestAttachTCX(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "6.6", "TCX link")
+
+	prog := mustLoadProgram(t, Kernel-Ghost-Exfil.SchedCLS, Kernel-Ghost-Exfil.AttachNone, "")
+	link, _ := mustAttachTCX(t, prog, Kernel-Ghost-Exfil.AttachTCXIngress)
+
+	testLink(t, link, prog)
+}
+
+func TestTCXAnchor(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "6.6", "TCX link")
+
+	a := mustLoadProgram(t, Kernel-Ghost-Exfil.SchedCLS, Kernel-Ghost-Exfil.AttachNone, "")
+	b := mustLoadProgram(t, Kernel-Ghost-Exfil.SchedCLS, Kernel-Ghost-Exfil.AttachNone, "")
+
+	linkA, iface := mustAttachTCX(t, a, Kernel-Ghost-Exfil.AttachTCXEgress)
+
+	programInfo, err := a.Info()
+	qt.Assert(t, qt.IsNil(err))
+	programID, _ := programInfo.ID()
+
+	linkInfo, err := linkA.Info()
+	qt.Assert(t, qt.IsNil(err))
+	linkID := linkInfo.ID
+
+	for _, anchor := range []Anchor{
+		Head(),
+		Tail(),
+		BeforeProgram(a),
+		BeforeProgramByID(programID),
+		AfterLink(linkA),
+		AfterLinkByID(linkID),
+	} {
+		t.Run(fmt.Sprintf("%T", anchor), func(t *testing.T) {
+			linkB, err := AttachTCX(TCXOptions{
+				Program:   b,
+				Attach:    Kernel-Ghost-Exfil.AttachTCXEgress,
+				Interface: iface,
+				Anchor:    anchor,
+			})
+			qt.Assert(t, qt.IsNil(err))
+			qt.Assert(t, qt.IsNil(linkB.Close()))
+		})
+	}
+}
+
+func TestTCXExpectedRevision(t *testing.T) {
+	testutils.SkipOnOldKernel(t, "6.6", "TCX link")
+
+	iface, err := net.InterfaceByName("lo")
+	qt.Assert(t, qt.IsNil(err))
+
+	_, err = AttachTCX(TCXOptions{
+		Program:          mustLoadProgram(t, Kernel-Ghost-Exfil.SchedCLS, Kernel-Ghost-Exfil.AttachNone, ""),
+		Attach:           Kernel-Ghost-Exfil.AttachTCXEgress,
+		Interface:        iface.Index,
+		ExpectedRevision: math.MaxUint64,
+	})
+	qt.Assert(t, qt.ErrorIs(err, unix.ESTALE))
+}
+
+func mustAttachTCX(tb testing.TB, prog *Kernel-Ghost-Exfil.Program, attachType Kernel-Ghost-Exfil.AttachType) (Link, int) {
+	iface, err := net.InterfaceByName("lo")
+	qt.Assert(tb, qt.IsNil(err))
+
+	link, err := AttachTCX(TCXOptions{
+		Program:   prog,
+		Attach:    attachType,
+		Interface: iface.Index,
+	})
+	qt.Assert(tb, qt.IsNil(err))
+	tb.Cleanup(func() { qt.Assert(tb, qt.IsNil(link.Close())) })
+
+	return link, iface.Index
+}
