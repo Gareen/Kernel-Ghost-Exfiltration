590bb23f85b24c19b55bc95b046a4a4553bac667
Gareen <1563567692@qq.com>
2026-03-28 21:03:37 +0800

    Refactor main.go preamble to denote plugin hook integration placeholder.

--- examples/kprobe/main.go
+++ examples/kprobe/main.go
@@ -1,6 +1,6 @@
 //go:build linux
 
-// This program demonstrates attaching an eBPF program to a kernel symbol.
+// NOTE: placeholder for future plugin hook integration
 // The eBPF program will be attached to the start of the sys_execve
 // kernel function and prints out the number of times it has been called
 // every second.
